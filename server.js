f37b0c58582a7272aaf6e98f831a5b28f06bc077
hainguyen2120 <49867220+hainguyen2120@users.noreply.github.com>
2019-08-09 16:14:16 +0700

    code

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(1234,function (err) {
+    console.log("server is running");
+});
+
+app.get("/",function (req, res) {
+    res.send("hom nay thu 7, van phong nghi lam");
+});
+
+app.get("/contact-us",function (req,res) {
+    res.sendFile(__dirname+"/views/contact-us.html");
+});
