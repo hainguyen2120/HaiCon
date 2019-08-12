9dd37739a2634c2845b4f9cf9d54a1c62df8bd01
hainguyen2120 <49867220+hainguyen2120@users.noreply.github.com>
2019-08-12 10:07:53 +0700

    90

--- server.js
+++ server.js
@@ -14,3 +14,12 @@ app.get("/",function (req, res) {
 app.get("/contact-us",function (req,res) {
     res.sendFile(__dirname+"/views/contact-us.html");
 });
+app.use(express.static("public"));
+
+app.listen(1234,function (err) {
+    console.log("server is running");
+});
+
+app.get("/create_account",function (req, res) {
+    res.sendFile(__dirname+"/views/create_account");
+});
